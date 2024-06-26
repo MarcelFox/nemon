bd7009ff2fb9c338c6f9399d356b2eeef9c92b9c
MarcelFox <marcelfox@live.com>
2024-06-25 22:15:18 -0300

    initialhtml structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { MainComponent } from './content/main/main.component';
+import { ExpensesComponent } from './content/expenses/expenses.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: MainComponent },
+    { path: 'expenses', component: ExpensesComponent },
+];
