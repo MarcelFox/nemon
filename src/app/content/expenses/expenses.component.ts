72a7ad9917525863281163b6846ed66ba5117781
MarcelFox <marcelfox@live.com>
2024-06-26 22:42:18 -0300

    adding material & including tables on expenses

--- src/app/content/expenses/expenses.component.ts
+++ src/app/content/expenses/expenses.component.ts
@@ -1,12 +1,50 @@
-import { Component } from '@angular/core';
+import { CommonModule, JsonPipe } from '@angular/common';
+import { Component, signal } from '@angular/core';
+import { MatTableModule } from '@angular/material/table';
+
+export interface Expenses {
+  id: number;
+  name: string;
+  value: number;
+  description: string;
+}
+
+const EXPENSES_DATA: Expenses[] = [
+  { id: 1, name: 'gasto 1', value: 10.00, description: 'gasto necessário com tal coisa' },
+  { id: 2, name: 'gasto 2', value: 10.00, description: 'gasto necessário com outra coisa' },
+  { id: 2, name: 'gasto 2', value: 10.00, description: 'gasto necessário com outra coisa' },
+  { id: 2, name: 'gasto 2', value: 10.00, description: 'gasto necessário com outra coisa' },
+  { id: 2, name: 'gasto 2', value: 10.00, description: 'gasto necessário com outra coisa' },
+  { id: 2, name: 'gasto 2', value: 10.00, description: 'gasto necessário com outra coisa' },
+  { id: 2, name: 'gasto 2', value: 10.00, description: 'gasto necessário com outra coisa' },
+  { id: 2, name: 'gasto 2', value: 10.00, description: 'gasto necessário com outra coisa' },
+  { id: 2, name: 'gasto 2', value: 10.00, description: 'gasto necessário com outra coisa' },
+  { id: 2, name: 'gasto 2', value: 10.00, description: 'gasto necessário com outra coisa' },
+  { id: 2, name: 'gasto 2', value: 10.00, description: 'gasto necessário com outra coisa' },
+  { id: 2, name: 'gasto 2', value: 10.00, description: 'gasto necessário com outra coisa' },
+  { id: 2, name: 'gasto 2', value: 10.00, description: 'gasto necessário com outra coisa' },
+  { id: 2, name: 'gasto 2', value: 10.00, description: 'gasto necessário com outra coisa' },
+  { id: 2, name: 'gasto 2', value: 10.00, description: 'gasto necessário com outra coisa' },
+  { id: 2, name: 'gasto 2', value: 10.00, description: 'gasto necessário com outra coisa' },
+  { id: 2, name: 'gasto 2', value: 10.00, description: 'gasto necessário com outra coisa' },
+  { id: 2, name: 'gasto 2', value: 10.00, description: 'gasto necessário com outra coisa' },
+  { id: 2, name: 'gasto 2', value: 10.00, description: 'gasto necessário com outra coisa' },
+]
+
+const EXPENSES_DATA_2: Expenses[] = [
+  { id: 1, name: 'gasto 1', value: 10.00, description: 'gasto necessário com tal coisa' }
+]
+
 
 @Component({
   selector: 'app-expenses',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, JsonPipe, MatTableModule],
   templateUrl: './expenses.component.html',
   styleUrl: './expenses.component.css'
 })
 export class ExpensesComponent {
-
+  dataSource = signal(EXPENSES_DATA)
+  dataSource2 = signal(EXPENSES_DATA_2)
+  displayedColumns: string[] = ['name', 'value', 'description'];
 }
