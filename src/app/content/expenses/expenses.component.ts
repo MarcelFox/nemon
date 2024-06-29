ffc98b243151de496320584737dbd5c41ccb62d0
MarcelFox <marcelfox@live.com>
2024-06-29 11:04:49 -0300

    linting and adding input instead of signal

--- src/app/content/expenses/expenses.component.ts
+++ src/app/content/expenses/expenses.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule, JsonPipe } from '@angular/common';
-import { Component, signal, computed } from '@angular/core';
+import { Component, computed, input } from '@angular/core';
 import { MatTableModule } from '@angular/material/table';
 
 export interface Expenses {
@@ -9,49 +9,51 @@ export interface Expenses {
 }
 
 const EXPENSES_DATA: Expenses[] = [
-  { id: 1, name: 'gasto 1', value: 10.00 },
-  { id: 2, name: 'gasto 2', value: 10.00 },
-  { id: 2, name: 'gasto 2', value: 10.00 },
-  { id: 2, name: 'gasto 2', value: 10.00 },
-  { id: 2, name: 'gasto 2', value: 10.00 },
-  { id: 2, name: 'gasto 2', value: 10.00 },
-  { id: 2, name: 'gasto 2', value: 10.00 },
-  { id: 2, name: 'gasto 2', value: 10.00 },
-  { id: 2, name: 'gasto 2', value: 10.00 },
-  { id: 2, name: 'gasto 2', value: 10.00 },
-  { id: 2, name: 'gasto 2', value: 10.00 },
-  { id: 2, name: 'gasto 2', value: 10.00 },
-  { id: 2, name: 'gasto 2', value: 10.00 },
-  { id: 2, name: 'gasto 2', value: 10.00 },
-  { id: 2, name: 'gasto 2', value: 10.00 },
-  { id: 2, name: 'gasto 2', value: 10.00 },
-  { id: 2, name: 'gasto 2', value: 10.00 },
-  { id: 2, name: 'gasto 2', value: 10.00 },
-  { id: 2, name: 'gasto 2', value: 10.00 },
-]
+  { id: 1, name: 'gasto 1', value: 10.0 },
+  { id: 2, name: 'gasto 2', value: 10.0 },
+  { id: 2, name: 'gasto 2', value: 10.0 },
+  { id: 2, name: 'gasto 2', value: 10.0 },
+  { id: 2, name: 'gasto 2', value: 10.0 },
+  { id: 2, name: 'gasto 2', value: 10.0 },
+  { id: 2, name: 'gasto 2', value: 10.0 },
+  { id: 2, name: 'gasto 2', value: 10.0 },
+  { id: 2, name: 'gasto 2', value: 10.0 },
+  { id: 2, name: 'gasto 2', value: 10.0 },
+  { id: 2, name: 'gasto 2', value: 10.0 },
+  { id: 2, name: 'gasto 2', value: 10.0 },
+  { id: 2, name: 'gasto 2', value: 10.0 },
+  { id: 2, name: 'gasto 2', value: 10.0 },
+  { id: 2, name: 'gasto 2', value: 10.0 },
+  { id: 2, name: 'gasto 2', value: 10.0 },
+  { id: 2, name: 'gasto 2', value: 10.0 },
+  { id: 2, name: 'gasto 2', value: 10.0 },
+  { id: 2, name: 'gasto 2', value: 10.0 },
+];
 
-const EXPENSES_DATA_2: Expenses[] = [
-  { id: 1, name: 'gasto 1', value: 10.00 }
-]
-
+const EXPENSES_DATA_2: Expenses[] = [{ id: 1, name: 'gasto 1', value: 10.0 }];
 
 @Component({
   selector: 'app-expenses',
   standalone: true,
   imports: [CommonModule, JsonPipe, MatTableModule],
   templateUrl: './expenses.component.html',
-  styleUrl: './expenses.component.css'
+  styleUrl: './expenses.component.css',
 })
 export class ExpensesComponent {
-  expensesData = signal(EXPENSES_DATA)
-  bonusData = signal(EXPENSES_DATA_2)
+  expensesData = input<Expenses[]>(EXPENSES_DATA);
+  bonusData = input<Expenses[]>(EXPENSES_DATA_2);
+
   displayedColumns: string[] = ['name', 'value'];
   displayedColumns2: string[] = ['total', 'value'];
 
-  totalExpenses = computed(() => this.expensesData().reduce((acc: number, cur: Expenses): number => {
-    return acc + cur.value;
-  }, 0))
-  totalBonus = computed(() => this.bonusData().reduce((acc: number, cur: Expenses): number => {
-    return acc + cur.value;
-  }, 0))
+  totalExpenses = computed(() =>
+    this.expensesData().reduce((acc: number, cur: Expenses): number => {
+      return acc + cur.value;
+    }, 0)
+  );
+  totalBonus = computed(() =>
+    this.bonusData().reduce((acc: number, cur: Expenses): number => {
+      return acc + cur.value;
+    }, 0)
+  );
 }
