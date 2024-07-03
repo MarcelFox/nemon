3d1965c31528366f5fa2c5240b875ec682494b1e
MarcelFox <marcelfox@live.com>
2024-07-03 10:27:11 -0300

    adding firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,6 +3,8 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -12,6 +14,6 @@ export const appConfig: ApplicationConfig = {
     },
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideAnimationsAsync(),
+    provideAnimationsAsync(), provideFirebaseApp(() => initializeApp({"projectId":"nemon-storage","appId":"1:388497713280:web:d1111e3abeecdd40cc9e65","storageBucket":"nemon-storage.appspot.com","apiKey":"AIzaSyBY8a9Uep9i-gslLvTOuwS4qj6Aj1KEh-k","authDomain":"nemon-storage.firebaseapp.com","messagingSenderId":"388497713280"})), provideFirestore(() => getFirestore()),
   ],
 };
