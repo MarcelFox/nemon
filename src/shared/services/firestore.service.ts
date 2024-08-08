e893b81c621f94fabcbe571b93f3987a2633083f
Marcel Fox <marcelfox@live.com>
2024-08-07 22:28:24 -0300

    implementing repository abstract class

--- src/shared/services/firestore.service.ts
+++ src/shared/services/firestore.service.ts
@@ -10,6 +10,7 @@ import {
   Firestore,
 } from '@angular/fire/firestore';
 import { Observable, first, from, map } from 'rxjs';
+import { Repository } from '../abstract/repository.abstract';
 
 /**
  * FirestoreService Class.
@@ -18,54 +19,29 @@ import { Observable, first, from, map } from 'rxjs';
 @Injectable({
   providedIn: 'root',
 })
-export class FirestoreService<Type> {
+export class FirestoreService<Type> implements Repository<Type> {
   constructor(@Inject('collectionName') private collectionName: string) {}
 
   private firestore = inject(Firestore);
   private collectionRef = collection(this.firestore, this.collectionName);
   public collection$: Observable<Type[]> = collectionData(this.collectionRef, { idField: 'id' });
 
-  /**
-   * Returns all documents from collection.
-   * @returns Observable<Type[]> containing all documents.
-   */
   public getAll(): Observable<Type[]> {
     return this.collection$;
   }
 
-  /**
-   * Get documents that have the same key and value as informed.
-   * @param key Key name from the object.
-   * @param value Value of the key.
-   * @returns Observable<Type[]> of document found.
-   */
   public getByKeyValue(key: string, value: unknown): Observable<Type[]> {
     return this.collection$.pipe(map((doc) => doc.filter((e) => (e as Record<string, unknown>)[key] === value)));
   }
 
-  /**
-   * Get a document by it's ID.
-   * @param docId Document id.
-   * @returns Observable<Type> containing the data found.
-   */
   public getById(docId: string): Observable<Type[]> {
     return this.getByKeyValue('id', docId);
   }
 
-  /**
-   * Create a new document into the firestore collection.
-   * @param data Document data to be created.
-   * @returns Observable<string> ID from created doc
-   */
   public create(data: Type): Observable<string> {
     return from(addDoc(this.collectionRef, { ...data, createdAt: new Date() }).then((reponse) => reponse.id));
   }
 
-  /**
-   * Updates the document data by it's id.
-   * @param docId ID from document to be updated.
-   * @param data Data to be updated.
-   */
   public update(docId: string, data: Type[]): void {
     // TODO: Consider  transaction instead of first() pipe operator.
     this.getById(docId)
@@ -75,11 +51,6 @@ export class FirestoreService<Type> {
       });
   }
 
-  /**
-   * Deletes a document by it's ID.
-   * @param docId Document ID
-   * @returns Observable<void>
-   */
   public delete(docId: string): Observable<void> {
     return from(deleteDoc(this.getDocRef(docId)));
   }
