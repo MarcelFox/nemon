5ae975062ae89674fbfb1691545b72381e4af9a3
Marcel Fox <marcelfox@live.com>
2024-08-07 21:24:03 -0300

    provinding FirestoreService

--- src/shared/components/expense-list/expense-list.component.ts
+++ src/shared/components/expense-list/expense-list.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FirestoreService } from '../../services/firestore.service';
+import { ExpensesCollection } from '../../../app/content/expenses/interfaces/Expenses.interface';
 
 @Component({
   selector: 'app-expense-list',
   standalone: true,
   imports: [],
   templateUrl: './expense-list.component.html',
-  styleUrl: './expense-list.component.css'
+  styleUrl: './expense-list.component.css',
+  providers: [
+    {
+      provide: 'firestore',
+      useFactory: () => new FirestoreService<ExpensesCollection>('expenses'),
+    },
+  ],
 })
-export class ExpenseListComponent {
-
+export class ExpenseListComponent implements OnInit {
+  constructor(private firestore: FirestoreService<ExpensesCollection>) {}
+  ngOnInit(): void {
+    console.log(this.firestore.getAll());
+  }
 }
