c562c23726055b3afdbeaf4fc77f1862574d8369
Marcel Fox <marcelfox@live.com>
2024-08-08 09:49:20 -0300

    adding dumb methods to be used on form

--- src/shared/components/expense-list/expense-list.component.ts
+++ src/shared/components/expense-list/expense-list.component.ts
@@ -1,12 +1,27 @@
-import { Component, Inject, OnInit, inject } from '@angular/core';
+import { Component, Inject, Input, OnInit, WritableSignal, computed, inject, signal } from '@angular/core';
 import { FirestoreService } from '../../services/firestore.service';
-import { ExpensesCollection } from '../../../app/content/expenses/interfaces/Expenses.interface';
+import { Expenses, ExpensesCollection } from '../../../app/content/expenses/interfaces/Expenses.interface';
 import { first, tap } from 'rxjs';
+import { LocalStorageService } from '../../services/local-storage.service';
+import { CommonModule, JsonPipe } from '@angular/common';
+import { MatTableModule } from '@angular/material/table';
+import { Sort, MatSortModule } from '@angular/material/sort';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-expense-list',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    JsonPipe,
+    MatTableModule,
+    MatSortModule,
+    MatInputModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './expense-list.component.html',
   styleUrl: './expense-list.component.css',
   providers: [
@@ -15,23 +30,65 @@ import { first, tap } from 'rxjs';
       useClass: FirestoreService<ExpensesCollection>,
     },
     {
-      provide: 'collectionName',
-      useValue: 'expenses',
+      provide: 'storageService',
+      useClass: LocalStorageService,
     },
   ],
 })
 export class ExpenseListComponent implements OnInit {
-  constructor(@Inject('expensesService') private expensesService: FirestoreService<ExpensesCollection>) {}
+  constructor(
+    @Inject('expensesService') private expensesService: FirestoreService<ExpensesCollection>,
+    @Inject('storageService') private storageService: LocalStorageService,
+    @Inject('title') public title: string
+  ) {}
+
+  // TODO: Remove type from ExpensesCollection to rely only on collection itself.
   ngOnInit(): void {
+    console.log(this.storageService.get('expenses'));
     this.expensesService
-      .getByKeyValue('type', 'expense')
-      .pipe(tap((e) => console.log(e)))
-      .pipe(first())
+      .getAll()
+      .pipe(
+        tap((collection) => {
+          const docs = collection.flatMap((e) => e.data);
+          this.data.update(() => docs);
+        })
+      )
       .subscribe();
-    this.expensesService
-      .getByKeyValue('type', 'bonus')
-      .pipe(tap((e) => console.log(e)))
-      .pipe(first())
-      .subscribe();
+  }
+
+  data: WritableSignal<Expenses[]> = signal([]);
+
+  expensesForm = new FormGroup({
+    detail: new FormControl(''),
+    value: new FormControl(0),
+  });
+
+  public totalSum = computed(() =>
+    this.data()
+      ? this.data().reduce((acc: number, cur: Expenses): number => {
+          return acc + cur.value;
+        }, 0)
+      : 0
+  );
+
+  /**
+   * Sorts the objects into the Material table.
+   * @param sort Sort from material, used to get direction.
+   * @param objectsList The objects to be sorted.
+   */
+  sortData(sort: Sort, objectsList: Expenses[]) {
+    objectsList.sort((a, b) => (sort.direction === 'asc' ? a.value - b.value : b.value - a.value));
+  }
+
+  removeElement(elementId: number): void {
+    console.log(`REMOVING: ${elementId}`);
+  }
+
+  onAddExpense(formGroupData: FormGroup, expenseData: WritableSignal<Expenses[]>, expense: string) {
+    console.log('lol');
+  }
+
+  onSave(expenseData: WritableSignal<Expenses[]>) {
+    console.log(`SAVING: ${expenseData}`);
   }
 }
