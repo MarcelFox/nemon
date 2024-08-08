d1a836d59a2930cc1e395146f328660d00286545
Marcel Fox <marcelfox@live.com>
2024-08-08 17:41:25 -0300

    adding monthtabs and adjustments

--- src/shared/components/expense-list/expense-list.component.ts
+++ src/shared/components/expense-list/expense-list.component.ts
@@ -1,8 +1,19 @@
-import { Component, Inject, Input, OnInit, WritableSignal, computed, inject, signal } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  Inject,
+  Input,
+  OnInit,
+  ViewChild,
+  WritableSignal,
+  computed,
+  inject,
+  signal,
+} from '@angular/core';
 import { FirestoreService } from '../../services/firestore.service';
 import { Expenses, ExpensesCollection } from '../../../app/content/expenses/interfaces/Expenses.interface';
 import { first, tap } from 'rxjs';
-import { LocalStorageService } from '../../services/local-storage.service';
+import { Timestamp } from 'firebase/firestore';
 import { CommonModule, JsonPipe } from '@angular/common';
 import { MatTableModule } from '@angular/material/table';
 import { Sort, MatSortModule } from '@angular/material/sort';
@@ -29,16 +40,11 @@ import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
       provide: 'expensesService',
       useClass: FirestoreService<ExpensesCollection>,
     },
-    {
-      provide: 'storageService',
-      useClass: LocalStorageService,
-    },
   ],
 })
 export class ExpenseListComponent implements OnInit {
   constructor(
     @Inject('expensesService') private expensesService: FirestoreService<ExpensesCollection>,
-    @Inject('storageService') private storageService: LocalStorageService,
     @Inject('title') public title: string
   ) {}
 
@@ -46,12 +52,12 @@ export class ExpenseListComponent implements OnInit {
   ngOnInit(): void {
     this.expensesService
       .getAll()
+      .pipe(first())
       .pipe(
         tap((collection) => {
-          collection = collection.filter((doc) => {
-            this.collectionData.update(() => [doc]);
-            return doc.createdAt.toDate().getMonth() === this.expenseDate.getMonth();
-          });
+          this.handleMonthsTab(collection);
+          collection = collection.filter((doc) => doc.createdAt.toDate().getMonth() === this.expenseDate.getMonth());
+          this.collectionData.update(() => collection);
           const docs = collection.flatMap((e) => e.data);
           this.data.update(() => docs);
           this.lastId.update(() => docs.length);
@@ -61,10 +67,14 @@ export class ExpenseListComponent implements OnInit {
   }
 
   @Input({ required: false }) expenseDate: Date = new Date();
+  @Input({ required: false }) hideMonthsTab: boolean = true;
+  @ViewChild('expenseValueInputRef') expensesValueInputRef!: ElementRef;
 
   data: WritableSignal<Expenses[]> = signal([]);
   lastId: WritableSignal<number> = signal(0);
   collectionData: WritableSignal<ExpensesCollection[]> = signal([]);
+  months: WritableSignal<{ id: number; date: Date }[]> = signal([]);
+  activeMonthId: WritableSignal<number> = signal(0);
   collectionId: string = '';
   formChanged: boolean = false;
 
@@ -80,6 +90,19 @@ export class ExpenseListComponent implements OnInit {
         }, 0)
       : 0
   );
+
+  handleMonthsTab(collection: ExpensesCollection[]) {
+    this.months.update(() =>
+      collection.map((e, id) => {
+        return { date: e.createdAt.toDate(), id };
+      })
+    );
+    const foundActualMonth = this.months().filter((month) => month.date.getMonth() === this.expenseDate.getMonth());
+    if (foundActualMonth.length < 1) {
+      this.months.update(() => [...this.months(), { id: this.months().length, date: this.expenseDate }]);
+    }
+    this.activeMonthId.update(() => this.months().length - 1);
+  }
 
   /**
    * Sorts the objects into the Material table.
@@ -106,12 +129,37 @@ export class ExpenseListComponent implements OnInit {
     this.lastId.update(() => this.lastId() + 1);
     this.data.update(() => [...this.data(), { ...(this.expensesForm.value as Expenses), id: this.lastId() }]);
     this.formChanged = true;
+    this.expensesForm.reset();
+    this.expensesValueInputRef.nativeElement.focus();
   }
 
-  saveExpense(expensesData: WritableSignal<Expenses[]>) {
+  saveExpense() {
     if (this.collectionData().length > 0) {
-      this.expensesService.update(this.collectionData()[0].id, { data: expensesData() });
-      this.formChanged = false;
+      this.expensesService.update(this.collectionData()[0].id, { data: this.data() });
+    } else {
+      this.expensesService.create({
+        createdAt: Timestamp.now(),
+        data: this.data(),
+        type: 'expenses',
+      } as ExpensesCollection);
     }
+    this.formChanged = false;
+  }
+
+  public onMonthClick(e: { id: number; date: Date }) {
+    this.activeMonthId.update(() => e.id);
+    this.expensesService
+      .getAll()
+      .pipe(first())
+      .pipe(
+        tap((collection) => {
+          collection = collection.filter((doc) => doc.createdAt.toDate().getMonth() === e.date.getMonth());
+          this.collectionData.update(() => collection);
+          const docs = collection.flatMap((e) => e.data);
+          this.data.update(() => docs);
+          this.lastId.update(() => docs.length);
+        })
+      )
+      .subscribe();
   }
 }
