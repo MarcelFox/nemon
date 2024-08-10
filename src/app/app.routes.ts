4cb140a7e6b0c5550b1e089d11d849fa8a915523
Marcel Fox <marcelfox@live.com>
2024-08-10 10:59:52 -0300

    adding profile component, route for it and adjusting sidebar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,11 @@
 import { Routes } from '@angular/router';
 import { MainComponent } from './content/main/main.component';
 import { ExpensesComponent } from './content/expenses/expenses.component';
+import { ProfileComponent } from './content/profile/profile.component';
+
 
 export const routes: Routes = [
     { path: '', component: MainComponent },
     { path: 'expenses', component: ExpensesComponent },
+    { path: 'profile', component: ProfileComponent },
 ];
